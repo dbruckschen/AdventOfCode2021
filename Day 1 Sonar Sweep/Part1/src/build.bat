9552500e388678d55a60e282faea4b6154e63910
David Bruckschen <d.bruckschen@gmail.com>
2021-12-09 22:46:29 +0100

    ...

--- Day 1 Sonar Sweep/Part1/src/build.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-set BUILDTOOLS="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" 
-call %BUILDTOOLS%
-
-cl.exe /Zi main.c /Fe: practice.exe 
-
-exit
-
